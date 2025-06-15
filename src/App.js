a210a7cf6e19a47ff8aad1117371360fb29c02eb
Krishnakant Kumar <krishnakantcuj@gmail.com>
2025-06-16 00:20:17 +0530

    Switch to HashRouter for GitHub Pages compatibility

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-import { Routes, Route } from 'react-router-dom'; import NavBar from './components/NavBar';
+import NavBar from './components/NavBar';
 import HeroSection from './components/HeroSection';
 import QuizSetupForm from './components/QuizSetupForm';
 import QuizPage from './components/QuizPage';
